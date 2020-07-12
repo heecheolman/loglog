9e2356bffb05b608ff978410b7b1b8fa99d52d62
HeeCheolKim <msn9975@gmail.com>
2020-07-13 00:51:37 +0900

    build: toc 1차작업

--- src/components/utterances.js
+++ src/components/utterances.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from 'react'
 
-const src = 'https://utteranc.es/client.js';
-const branch = 'master';
+const src = 'https://utteranc.es/client.js'
+const branch = 'master'
 
 export const Utterences = ({ repo }) => {
-  const rootElm = React.createRef();
+  const rootElm = React.createRef()
 
   useEffect(() => {
-    const utterances = document.createElement('script');
+    const utterances = document.createElement('script')
     const utterancesConfig = {
       src,
       repo,
@@ -15,13 +15,13 @@ export const Utterences = ({ repo }) => {
       async: true,
       'issue-term': 'pathname',
       crossorigin: 'anonymous',
-    };
+    }
 
     Object.keys(utterancesConfig).forEach(configKey => {
       utterances.setAttribute(configKey, utterancesConfig[configKey])
-    });
+    })
     rootElm.current.appendChild(utterances)
-  }, []);
+  }, [])
 
   return <div className="utterences" ref={rootElm} />
-};
+}
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import './blog-post.scss'
 import TagLabel from '../components/tag-label'
 import Divider from '../components/divider'
+import Toc from '../components/toc'
 import { graphql } from 'gatsby'
 import { Utterences } from '../components/utterances'
 import { useSiteMetadata } from '../hooks/use-site-metadata'
@@ -11,10 +12,26 @@ import { useSiteMetadata } from '../hooks/use-site-metadata'
 export default function Template({ data }) {
   const { markdownRemark: post } = data
   const { utterenceRepo } = useSiteMetadata()
+  const [fixToc, setFixToc] = useState(false)
 
   const contentImage = post.frontmatter.image
     ? post.frontmatter.image.childImageSharp.resize.src
     : ''
+
+  useEffect(() => {
+    const scrollHandler = event => {
+      if (window.pageYOffset >= 270) {
+        setFixToc(true)
+      } else {
+        setFixToc(false)
+      }
+    }
+
+    window.addEventListener('scroll', scrollHandler)
+    return () => window.removeEventListener('scroll', scrollHandler)
+  }, [fixToc])
+
+  const { headings } = post
 
   return (
     <Layout>
@@ -24,7 +41,7 @@ export default function Template({ data }) {
         image={contentImage}
       />
       <div className="post-section">
-        <div className="post-section-title">{post.frontmatter.title}</div>
+        <h1 className="post-section-title">{post.frontmatter.title}</h1>
         <div className="post-section-frontmatter">
           <span>{post.frontmatter.date}</span>
           <Divider type="vertical" />
@@ -33,6 +50,7 @@ export default function Template({ data }) {
           <span>{post.fields.readingTime.text}</span>
         </div>
         <Divider />
+        <Toc headings={headings} fixed={fixToc} />
         <div
           className="markdown-content"
           dangerouslySetInnerHTML={{ __html: post.html }}
@@ -58,20 +76,11 @@ export const pageQuery = graphql`
         title
         tags
         author
-        # image {
-        #   id
-        #   childImageSharp {
-        #     id
-        #     resize(width: 768) {
-        #       src
-        #       tracedSVG
-        #       width
-        #       height
-        #       aspectRatio
-        #       originalName
-        #     }
-        #   }
-        # }
+      }
+      headings {
+        id
+        depth
+        value
       }
       fields {
         readingTime {
