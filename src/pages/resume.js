8b8eb68b17cc9480ac4c597415d0de90b84efb05
HeeCheolKim <msn9975@gmail.com>
2022-02-05 17:05:43 +0900

    update: resume description 업데이트

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -7,11 +7,14 @@ import Experience from '../resume-components/Experience'
 import Likes from '../resume-components/Likes'
 import Education from '../resume-components/Education'
 import Seo from '../components/Seo'
+import useResume from '../hooks/useResume'
 
 function Resume() {
+  const resume = useResume()
+
   return (
     <ResumeLayout>
-      <Seo title="이력서" />
+      <Seo title="이력서" description={resume.description} />
       <article>
         <Introduce />
         <Career />
